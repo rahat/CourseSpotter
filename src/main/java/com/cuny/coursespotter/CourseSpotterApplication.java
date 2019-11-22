86930f4958ee56a65314d9c64a3330d9204689a4
Rahat Mahmud <rahat.mahmud@macaulay.cuny.edu>
2019-11-21 22:14:03 -0500

    Added project files

---
+++ src/main/java/com/cuny/coursespotter/CourseSpotterApplication.java
@@ -0,0 +1,13 @@
+package com.cuny.coursespotter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpotterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpotterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cuny/coursespotter/CourseSpotterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuny.coursespotter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpotterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
